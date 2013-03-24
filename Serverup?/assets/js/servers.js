f85e646dd52832117fa530a864a73901f7231928
Rajib Chowdhury <chowdhury284@gmail.com>
2013-03-25 01:34:48 +0530

    server templating stuffs done.

--- Serverup?/assets/js/servers.js
+++ Serverup?/assets/js/servers.js
@@ -1,9 +1,26 @@
 $(function () {
+	Handlebars.registerHelper("status_class", function(status){
+	   if (status == 'up') {
+	   		return 'label-success'
+	   } else {
+	   		return 'label-important'
+	   };
+	});
+
 	$.ajax({
 	  url: 'http://localhost:3000/api/v1/servers.json',
 	  data: { auth_token: localStorage.getItem("auth_token") },
 	  success: function (json) {
 	  	console.log(json)
+	  	var source = $("#listServersTemplate").html();
+	  	var template = Handlebars.compile(source);
+	  	$("#listServers").html(template(json));
+	  	$("span.timeago").timeago(); // apply fuzzy time
 	  }
 	});
+});
+
+$('#refresh').click(function(e) {
+	window.location.replace('servers.html');
+	e.preventDefault();
 });
