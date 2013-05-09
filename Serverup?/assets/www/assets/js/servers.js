a24f5b9fa9a332c80d601b27c4a9ebe0bc0f18ab
Rajib Chowdhury <chowdhury284@gmail.com>
2013-05-09 11:54:08 +0530

    Update servers.js

--- Serverup?/assets/www/assets/js/servers.js
+++ Serverup?/assets/www/assets/js/servers.js
@@ -1,27 +1,27 @@
 $(function () {
-	Handlebars.registerHelper("status_class", function(status){
-	   if (status == 'up') {
-	   		return 'label-success'
-	   } else {
-	   		return 'label-important'
-	   };
-	});
+    Handlebars.registerHelper("status_class", function(status){
+       if (status == 'up') {
+            return 'label-success'
+       } else {
+            return 'label-important'
+       };
+    });
 
-	$.ajax({
-	  url: 'http://serverup.io/api/v1/servers.json',
-	  data: { auth_token: localStorage.getItem("auth_token") },
-	  success: function (json) {
-	  	console.log(json)
-	  	var source = $("#listServersTemplate").html();
-	  	var template = Handlebars.compile(source);
-	  	$("#listServers").html(template(json));
-	  	$("span.timeago").timeago(); // apply fuzzy time
-	  	$("#listServersView").listview(); // apply mobile lisview
-	  }
-	});
+    $.ajax({
+      url: 'http://serverup.io/api/v1/servers.json',
+      data: { auth_token: localStorage.getItem("auth_token") },
+      success: function (json) {
+        console.log(json)
+        var source = $("#listServersTemplate").html();
+        var template = Handlebars.compile(source);
+        $("#listServers").html(template(json));
+        $("span.timeago").timeago(); // apply fuzzy time
+        $("#listServersView").listview(); // apply mobile lisview
+      }
+    });
 });
 
 $('#refresh').click(function(e) {
-	window.location.replace('servers.html');
-	e.preventDefault();
+    window.location.replace('servers.html');
+    e.preventDefault();
 });
